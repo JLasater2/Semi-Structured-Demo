e6c2a718a1a7c3fd39a9340d7fbbba239e5f97b4
JohnD <john.lasater.2@gmail.com>
2023-04-06 17:36:31 -0500

    Set file stage format to jsobn to resolve query issues.

--- snowflake/03_query_stage.sql
+++ snowflake/03_query_stage.sql
@@ -1,7 +1,16 @@
 use role semi_structured_demo;
 use WAREHOUSE compute_wh;
+use schema semi_structured_demo.stg;
 
-
-
+-- See what the raw data looks like
 select $1
-from @s3_stage/import/application_log.json
+from @s3_stage/import/application_log.json;
+
+-- View all of the keys in the JSON
+
+select y.*
+from (
+    select TO_VARIANT($1) as var_data
+    from @s3_stage/import/application_log.json
+)x ,
+lateral FLATTEN(var_data, recursive=>true) y
