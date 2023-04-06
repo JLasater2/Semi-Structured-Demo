65913c9723640abf89681342eae15f32898f07d5
JohnD <john.lasater.2@gmail.com>
2023-04-05 23:27:27 -0500

    Fix access_key bug

--- faker/s3_upload.py
+++ faker/s3_upload.py
@@ -5,8 +5,8 @@ with open('..\Common\s_python_s3_poc_accessKeys.csv', 'r') as csvfile:
     csvreader = csv.reader(csvfile)
     for row in csvreader:
         # Set your AWS access keys
-        ACCESS_KEY = row[0]
-        SECRET_KEY = row[1]
+        ACCESS_KEY = row[1]
+        SECRET_KEY = row[2]
 
 # Set the name of the S3 bucket
 BUCKET_NAME = 'sf-bucket-290'
@@ -19,6 +19,6 @@ S3_OBJECT_KEY = 'snowflake/import/application_log.json'
 s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY)
 
 # Upload the file to S3
-# s3.upload_file(LOCAL_FILE_PATH, BUCKET_NAME, S3_OBJECT_KEY)
+s3.upload_file(LOCAL_FILE_PATH, BUCKET_NAME, S3_OBJECT_KEY)
 
 print('File uploaded successfully!')
