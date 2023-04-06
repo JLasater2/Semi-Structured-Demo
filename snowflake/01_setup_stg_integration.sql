c51b836737c62aba151bcaf605fb89b2649e8f23
JohnD <john.lasater.2@gmail.com>
2023-04-06 12:40:29 -0500

    Not sure what changed

--- snowflake/01_setup_stg_integration.sql
+++ snowflake/01_setup_stg_integration.sql
@@ -1,7 +1,7 @@
 -- requires accountadmin 
 -- requires aws configuration of policy, role, and bucket
 use role accountadmin;
-create or replace storage integration s3_integration 
+create storage integration if not exists s3_integration 
     type = external_stage
     storage_provider = 'S3'
     storage_aws_role_arn = 'arn:aws:iam::168127582357:role/Snowflake_Role'
@@ -14,6 +14,3 @@ desc integration s3_integration;
 -- Confirm storage integration has been created
 show storage integrations;
 desc integration s3_integration;
-
-use role securityadmin;
-grant usage on integration s3_integration to role semi_structured_demo;
