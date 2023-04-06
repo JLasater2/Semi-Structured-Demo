31b5dd536118cffe7cc165bfbd28a0a1ad6ba7b9
JohnD <john.lasater.2@gmail.com>
2023-04-06 18:01:48 -0500

    Add firewall log to uploader

--- snowflake/03_query_stage.sql
+++ snowflake/03_query_stage.sql
@@ -9,10 +9,12 @@ from @s3_stage/import/application_log.json;
 -- View all of the keys in the JSON
 
 select 
-    distinct REGEXP_REPLACE(y.path, '\\[[0-9]+\\]', '[]') AS "Path"
+    REGEXP_REPLACE(y.path, '\\[[0-9]+\\]', '[]') AS "Path"
+    , count(1) as cnt
    -- y.*
 from (
     select TO_VARIANT($1) as var_data
     from @s3_stage/import/application_log.json
 )x ,
 lateral FLATTEN(var_data, recursive=>true) y
+group by 1
