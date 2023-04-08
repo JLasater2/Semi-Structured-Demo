6316c011ff0152556123851cb055b58720b09399
JohnD <john.lasater.2@gmail.com>
2023-04-07 23:44:12 -0500

    Convert json files to xml

--- faker/json_to_other_formats.py
+++ faker/json_to_other_formats.py
@@ -1,7 +1,7 @@
-
 import json
 import pyarrow as pa
 import pyarrow.parquet as pq
+import xml.etree.ElementTree as ET
 
 # Read the JSON file into a list of dictionaries
 with open('faker/output/application_log.json', 'r') as f:
@@ -13,3 +13,15 @@ table = pa.Table.from_pydict({k: [d.get(k, None) for d in data] for k in data[0]
 # Write the PyArrow Table to a Parquet file
 with open('faker/output/application_log.parquet', 'wb') as f:
     pq.write_table(table, f)
+
+# Generate an XML document from the same data
+root = ET.Element('application_logs')
+for d in data:
+    log_elem = ET.SubElement(root, 'log')
+    for k, v in d.items():
+        field_elem = ET.SubElement(log_elem, k)
+        field_elem.text = str(v)
+
+# Write the XML document to a file
+tree = ET.ElementTree(root)
+tree.write('faker/output/application_log.xml', encoding='utf-8', xml_declaration=True)
