7971d13f28528112295d1a1de334c5d0fe184815
JohnD <john.lasater.2@gmail.com>
2023-04-08 16:10:16 -0500

    Convert spaces to underscores for xml tag names.

--- faker/json_to_other_formats.py
+++ faker/json_to_other_formats.py
@@ -8,7 +8,7 @@ with open('faker/output/application_log.json', 'r') as f:
     data = json.load(f)
 
 # Convert the list of dictionaries to a PyArrow Table
-table = pa.Table.from_pydict({k: [d.get(k, None) for d in data] for k in data[0]})
+table = pa.Table.from_pydict({k.replace(' ', '_'): [d.get(k, None) for d in data] for k in data[0]})
 
 # Write the PyArrow Table to a Parquet file
 with open('faker/output/application_log.parquet', 'wb') as f:
@@ -19,7 +19,7 @@ root = ET.Element('application_logs')
 for d in data:
     log_elem = ET.SubElement(root, 'log')
     for k, v in d.items():
-        field_elem = ET.SubElement(log_elem, k)
+        field_elem = ET.SubElement(log_elem, k.replace(' ', '_'))
         field_elem.text = str(v)
 
 # Write the XML document to a file
