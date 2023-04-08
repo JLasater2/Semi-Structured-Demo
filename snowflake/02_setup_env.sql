806a8eaa18afee61fd6be13b4ea48367066d0383
JohnD <john.lasater.2@gmail.com>
2023-04-07 20:07:58 -0500

    add comments

--- snowflake/02_setup_env.sql
+++ snowflake/02_setup_env.sql
@@ -4,21 +4,19 @@ create or replace role semi_structured_demo;
 grant role semi_structured_demo to role sysadmin;
 grant role semi_structured_demo to user johnlasater2;
 
--- setup 
+-- setup integration
 use role accountadmin;
 grant usage on integration s3_integration to role semi_structured_demo; 
 
+-- setup db & schema
 use role sysadmin;
 create or replace database semi_structured_demo;
 grant all on database semi_structured_demo to role semi_structured_demo;
 grant usage on warehouse compute_wh to role semi_structured_demo;
-
 use role semi_structured_demo;
 create or replace schema semi_structured_demo.stg;
 
 -- setup stage
--- create external stage
-
 create or replace stage semi_structured_demo.stg.s3_stage 
     url = 's3://sf-bucket-290/snowflake/'
     storage_integration = s3_integration
