2ead32b8df73294e7221a22c5d2ab8fbe1491a80
JohnD <john.lasater.2@gmail.com>
2023-04-10 23:04:32 -0500

    Cleanup scripts, add faker read only perms

---
+++ snowflake/03_setup_database.sql
@@ -0,0 +1,25 @@
+-- setup schema
+use role semi_structured_demo;
+create or replace schema semi_structured_demo.stg;
+
+-- setup stage
+create or replace stage semi_structured_demo.stg.s3_stage_json
+    url = 's3://sf-bucket-290/snowflake/'
+    storage_integration = s3_integration
+    comment = 'external s3 stage for importing semi-structured data'
+    directory = (enable = true)
+    file_format = (type = json strip_outer_array = true file_extension = '.json')
+    ;
+
+create or replace stage semi_structured_demo.stg.s3_stage_xml
+    url = 's3://sf-bucket-290/snowflake/'
+    storage_integration = s3_integration
+    comment = 'external s3 stage for importing semi-structured data'
+    directory = (enable = true)
+    file_format = (type = xml)
+    ;
+
+-- show stage properties
+desc stage stg.s3_stage_json;
+-- list files available in the stage
+list @s3_stage_json/import/;
