cd6cbb6973711d3e787ff0387d2030134354ae5b
JohnD <john.lasater.2@gmail.com>
2023-04-12 22:15:36 -0500

    Add account numbers to event message.  Restrict random dates to recent date range.

--- faker/01_application_log.py
+++ faker/01_application_log.py
@@ -1,12 +1,19 @@
 from faker import Faker
 import json
 import random
+import datetime
 
 # Instantiate the Faker object
 fake = Faker()
 
 # Create a list to store the application logs
 app_logs = []
+
+# Define the date range for the logs
+start_date = datetime.date(2023, 4, 1)
+end_date = datetime.date(2023, 4, 10)
+
+event_description = ''
 
 # Set the number of logs to generate
 num_logs = 100
@@ -16,10 +23,10 @@ for i in range(num_logs):
     # Generate a random event type
     event_type = random.choice(['Info', 'Warning', 'Error'])
 
-    if event_type == 'Error':
-        error_message = "Login failed"
-    else:
-        error_message = ''
+    if event_type == "Error":
+        event_description = "Login failed"
+    elif event_type == "Info":
+        event_description = "Account profile -  account " + str(random.randint(100000000, 9999999999))
 
     # Generate a random username
     username = fake.user_name()
@@ -29,14 +36,14 @@ for i in range(num_logs):
 
     # Create a dictionary to represent the application log
     app_log = {
-        'Date': fake.date(),
+        'Date': str(fake.date_between_dates(date_start=start_date, date_end=end_date)),
         'Time': fake.time(),
         'Application': 'Sales System',
         'Event Type': event_type,
-        'Event Description': fake.sentence(),
+        'Event Description': event_description,
         'Username': username,
-        'IP Address': ip_address,
-        'Error Message': error_message
+        #'IP Address': ip_address,
+        #'Error Message': error_message
     }
 
     # Append the application log dictionary to the list
