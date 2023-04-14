8fbf876410dfc3b5ae5b580cd1195c7d45fa10ae
JohnD <john.lasater.2@gmail.com>
2023-04-14 01:10:47 -0500

    Enrich app log data

--- faker/01_application_log.py
+++ faker/01_application_log.py
@@ -20,13 +20,6 @@ num_logs = 100
 
 # Loop through the number of logs to generate
 for i in range(num_logs):
-    # Generate a random event type
-    event_type = random.choice(['Info', 'Warning', 'Error'])
-
-    if event_type == "Error":
-        event_description = "Login failed"
-    elif event_type == "Info":
-        event_description = "Account profile -  account " + str(random.randint(100000000, 9999999999))
 
     # Define the choices for the event types and their corresponding probabilities
     event_type_choices = [
@@ -47,26 +40,47 @@ for i in range(num_logs):
 
     # Generate a random IP address
     ip_address = fake.ipv4()
-    
-    # Generate fake currency amount between two amounts
-    min_amount = 10000.50
-    max_amount = 999000.25
-    current_balance = round(fake.random.uniform(min_amount, max_amount), 2)
 
     # Create a dictionary to represent the application log
     app_log = {
         'Date': str(fake.date_between_dates(date_start=start_date, date_end=end_date)),
         'Time': fake.time(),
-        'Application':  'CustInsight360', #random.choice(['FraudSentry', 'C360', 'DocuViewer']),
-        'Event Type': event_type,
         'Username': username,
-        'Current Balance': current_balance
+        'Application':  'CustInsight360', 
+        'Event Type': event_type
     }
 
-    # Add the account number if the event type is 'Account View'
+    # Add account details if the event type is 'Account View'
     if event_type == 'Account View':
+        # Set random account type
         app_log['Event Details'] = {}
+        app_log['Event Details']['Account Type'] = random.choice(['Platinum Checking', 'Premier Savings', 'Advantage Money Market'])
+
+        # Generate fake acct number
         app_log['Event Details']['Account Number'] = str(random.randint(100000000, 9999999999))
+
+        # Generate fake currency amount between two amounts
+        min_amount = 10000.50
+        max_amount = 999000.25
+        current_balance = round(fake.random.uniform(min_amount, max_amount), 2)
+        app_log['Event Details']['Current Balance'] = current_balance
+
+    # Add customer details if the event type is 'Cust Profile View'
+    if event_type == 'Cust Profile View':
+        app_log['Event Details'] = {}
+        app_log['Event Details']['Customer ID']  = 'TX-' + str(random.randint(100000000, 9999999999))
+
+    # Add customer details if the event type is 'Cust Profile Maint'
+    if event_type == 'Cust Profile Maint':
+        app_log['Event Details'] = {}
+        app_log['Event Details']['Primary Phone']  = {}
+        app_log['Event Details']['Primary Phone']['Previous Value'] = fake.phone_number()
+        app_log['Event Details']['Primary Phone']['New Value'] = fake.phone_number()
+
+    # Add customer details if the event type is 'Account Maint'
+    if event_type == 'Account Maint':
+        app_log['Event Details'] = {}
+        app_log['Event Details']['Add authorized signor'] = 'TX-' + str(random.randint(100000000, 9999999999))
 
     # Append the application log dictionary to the list
     app_logs.append(app_log)
