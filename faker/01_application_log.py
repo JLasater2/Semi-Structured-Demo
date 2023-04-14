611e2e3b986ac23c97556ac66146322112284a66
JohnD <john.lasater.2@gmail.com>
2023-04-14 00:29:29 -0500

    Change event types.  Make event dtls conditional.

--- faker/01_application_log.py
+++ faker/01_application_log.py
@@ -28,6 +28,20 @@ for i in range(num_logs):
     elif event_type == "Info":
         event_description = "Account profile -  account " + str(random.randint(100000000, 9999999999))
 
+    # Define the choices for the event types and their corresponding probabilities
+    event_type_choices = [
+        ('Cust Profile Maint', 0.1),
+        ('Cust Profile View', 0.5),
+        ('Account Maint', 0.1),
+        ('Account View', 0.3)
+    ]
+
+    # Create a weighted list of event types
+    event_types = [c[0] for c in event_type_choices for i in range(int(c[1]*10))]
+
+    # Randomly select an event type from the weighted list
+    event_type = random.choice(event_types)
+
     # Generate a random username
     username = fake.user_name()
 
@@ -43,12 +57,16 @@ for i in range(num_logs):
     app_log = {
         'Date': str(fake.date_between_dates(date_start=start_date, date_end=end_date)),
         'Time': fake.time(),
-        'Application': 'Sales System',
+        'Application':  'CustInsight360', #random.choice(['FraudSentry', 'C360', 'DocuViewer']),
         'Event Type': event_type,
-        'Event Description': event_description,
         'Username': username,
         'Current Balance': current_balance
     }
+
+    # Add the account number if the event type is 'Account View'
+    if event_type == 'Account View':
+        app_log['Event Details'] = {}
+        app_log['Event Details']['Account Number'] = str(random.randint(100000000, 9999999999))
 
     # Append the application log dictionary to the list
     app_logs.append(app_log)
