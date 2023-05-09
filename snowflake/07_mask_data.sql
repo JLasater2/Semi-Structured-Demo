1cc542d0e61498fe68d51684e86907592e8f2d93
JohnD <john.lasater.2@gmail.com>
2023-05-08 21:45:26 -0500

    Rename files, add masking to view

--- snowflake/07_mask_data.sql
+++ snowflake/07_mask_data.sql
@@ -6,20 +6,33 @@ use schema semi_structured_demo.stg;
 -- Need external table?
 -- Could partially expose data and only mask sensitive fields
 
+-- Masking policy example 1 - mask JSON data directly
+-- Note the variant parameter and return types
+-- Have to run in Snowsight?
+-- create or replace masking policy variant_test_mask
+-- as (val variant) returns variant ->
+-- case
+--    when is_role_in_session('sensitive_read_only') then val
+--    else object_insert(val, 'Account Number', '***', true):: variant  -- Case sensitive?
+-- end
+-- ;
 
-CREATE OR REPLACE MASKING POLICY acct_no_mask AS (key string, val string) RETURNS string ->
-  CASE
-    WHEN 
-      CURRENT_ROLE() IN ('SENSITIVE_READ_ONLY') 
-      and key = 'Account Number'
-    THEN val
-    ELSE '*********'
-  END 
-  
-  ;
+--   We can do this without defining a structure for the JSON
+-- create or replace masking policy variant_test_mask
+-- as (val variant) returns variant ->
+-- case
+--    when is_role_in_session('sensitive_read_only') then val
+--    else object_insert(val, 'Account Number', '***', true):: variant  -- Case sensitive?
+-- end
+-- ;
 
-  ALTER VIEW v_application_log MODIFY COLUMN email SET MASKING POLICY email_mask
-  ;
+-- create or replace view v_application_log_raw as 
+--     select $1 as var_data, $1 as var_data_unmasked
+--     from @s3_stage_json/import/application_log.json
+;
+--  alter view v_application_log_raw modify column var_data 
+--  set masking policy variant_test_mask ;
 
-  -- show roles
-  -- ;
+-- select var_data_unmasked, var_data_unmasked:"Account Number"
+-- from v_application_log_raw
+-- ;
