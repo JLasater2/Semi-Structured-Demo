1ca9d0ac9cac44c22a34069c1875e3be60cf963f
JohnD <john.lasater.2@gmail.com>
2023-05-22 05:07:03 -0500

    Major updates to app log; pivot view from rows to columns

--- snowflake/04_profile_application_log.sql
+++ snowflake/04_profile_application_log.sql
@@ -3,73 +3,89 @@
     use warehouse compute_wh;
     use schema semi_structured_demo.stg;
 
+/*  Step 1: View raw data 
 
--- 1) see what the raw data looks like - json
---    note - 1 json object per row since we used "strip_outer_array"
+    Let's see what the raw data looks like - json
+    Note the following: 
+        1) There's one json object per row since we used "strip_outer_array"
+        2) In Snowflake, the JSON is newline delimited (ND) and therefore is easier to read than the non-ND raw file
+        3) There appear to be structual differences between the records; not every record follows the same format
+ */
     select $1
     from @s3_stage_json/import/application_log.json
     limit 10;
 
+/*  Step 2: View distinct keys
 
--- 2) use lateral flatten() to view all of the unique paths in the json
---    use recursive otherwise it will just flatten the top level
---    note - key names are in [brackets] if the key name contains a space
---    order by will "group" related objects together
+    We will use lateral flatten() to get a distinct list of keys and key paths within the file
+    to give us a better idea of what the structure looks like.
+    Note the following:
+        1) The recursive option of lateral flatten() is set to true.  Otherwise the function
+           would only flatten the top level object and not any nested arrays or objects.
+        2) Lateral flatten has moved each key onto a separate row, even for nested objects like 
+           "Event Details" and arrays like "Recent Txns."
+        3) Anything with IS_ARRAY = True is a candidate for normalizing 
+        4) Note which records are flagged true for IS_OBJECT and IS_ARRAY; this will be relevant in a later step.
+*/
     select 
-        regexp_replace(y.path, '\\[[0-9]+\\]', '[]') as "path"  -- use regex to remove array index to allow for correct grouping
-        , count(1) as cnt
-    from (
-        select $1 as var_data
-        from @s3_stage_json/import/application_log.json
-    )x ,
-    lateral flatten(var_data, recursive => true) y
-    group by 1
-    order by 1
+        count(1) as cnt
+        , regexp_replace(y.path, '\\[[0-9]+\\]', '[]') as "path"  -- use regex to remove array index to allow for correct grouping
+        , is_object(y.value)
+        , is_array(y.value)
+    from  @s3_stage_json/import/application_log.json x ,
+    lateral flatten( x.$1, recursive => true ) y
+    group by 2, 3, 4
+    order by 2
     ;
 
-
--- 3) now that we have an idea of they keys
---    remove the group by to view the values
---    note that some of the values contain arrays or objects rather than simple values 
+/* Step 4: View flattened data
+   
+   Now that we have an idea of they keys, we'll remove the group by to view the values.
+   Note the following:
+     1) The SEQ column indicates the row number in the original file that each row corresponds to.
+     2) A row is created for each key/value pair in the outermost object 
+*/
     select 
         y.*
-        -- , is_object(y.value) as is_object
-        -- , is_array(y.value) as is_array
-    from (
-        select $1 as var_data
-        from @s3_stage_json/import/application_log.json
-    )x ,
-    lateral flatten( var_data, recursive => true ) y
+        , is_object(y.value) as is_object
+        , is_array(y.value) as is_array
+    from  @s3_stage_json/import/application_log.json x ,
+    lateral flatten( x.$1:"Event Details", recursive => true ) y
+    limit 100
 ;
--- 4) use is_object() to filter out the rows where the value contains other json rather than
---    a simple value.  Note there appears to be sensitive data such as account numbers
-    select 
-        y.*
-    from (
-        select $1 as var_data
-        from @s3_stage_json/import/application_log.json
-    )x ,
-    lateral flatten( var_data, recursive => true ) y
-    where 
-        is_object(y.value) = false  -- use this filter to ignore rows that are not flattened
-        and is_array(y.value) = false
-    ;
-
--- 5) create a view and apply data masking to protect sensitive data
--- 5) We can convert this query to a view to make the data easier to use for downstream consumers
+-- -- 4) use is_object() to filter out the rows where the value contains other json rather than
+-- --    a simple value.  Note there appears to be sensitive data such as account numbers
+--     select 
+--         y.*
+--     from (
+--         select $1 as var_data
+--         from @s3_stage_json/import/application_log.json
+--     )x ,
+--     lateral flatten( var_data, recursive => true ) y
+--     where 
+--         is_object(y.value) = false  -- use this filter to ignore rows that are not flattened
+--         and is_array(y.value) = false
+--     ;
+/* Step 5: Create view 
+   
+   Now that we have an idea of they keys, we can create a view at the grain of the original file.
+   This will make the data easier to work with for downstream consumers.
+   Note the following:
+     1) The SEQ column indicates the row number in the original file that each row corresponds to.
+     2) A row is created for each key/value pair in the outermost object 
+*/
 create or replace view v_application_log as 
     select 
-        y.key :: varchar as key
-        , y.value :: varchar as value
-        , y.path :: varchar as path
-    from (
-        select $1 as var_data
-        from @s3_stage_json/import/application_log.json
-    )x ,
-    lateral flatten( var_data, recursive => true ) y
-    where 
-        is_object(y.value) = false  -- use this filter to ignore rows that are not flattened
-        and is_array(y.value) = false
+        x.$1:Application :: varchar as Application
+        , to_timestamp(x.$1:Date || ' ' || x.$1:Time) as Timestamp
+        , x.$1:Username :: varchar as UserName
+        , x.$1:"Event Type" :: varchar as EventType
+        , x.$1:"Event Details"."Primary Phone"."Previous Value" :: varchar as PhonePreviousValue
+        , x.$1:"Event Details"."Primary Phone"."New Value" :: varchar as PhoneNewValue
+        , x.$1['Event Details']['Account Type'] :: varchar as AccountType
+        , x.$1['Event Details']['Account Number'] :: varchar as AccountNumber
+        , x.$1['Event Details']['Recent Txns'] as RecentTxns  -- This is still an array and can be flattened to denormalize the data
+   from @s3_stage_json/import/application_log.json x
     ;
 
     create or replace masking policy account_mask
@@ -80,8 +96,12 @@ create or replace view v_application_log as
         end
     ;
 
-    alter view v_application_log modify column value 
-        set masking policy account_mask using (value, key);
+    -- alter view v_application_log modify column value 
+    --     set masking policy account_mask using (value, key);
 
     -- Test the view 
-    select * from v_application_log;
+    select * 
+    from v_application_log
+    limit 10
+
+;
