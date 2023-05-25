a8f04c094ab5e6933ec85266488e68c913379fc6
JohnD <john.lasater.2@gmail.com>
2023-05-24 22:40:21 -0500

    Add end date to sec event log view

--- snowflake/04_profile_security_event_log.sql
+++ snowflake/04_profile_security_event_log.sql
@@ -18,16 +18,29 @@ order by 1
 
 -- Create view to simplify further analysis
  create or replace view v_security_event_log as 
+    with base as (
+        select 
+            security_event_log.$1:"Action" :: varchar as Action
+            , to_timestamp(security_event_log.$1:Date || ' ' || security_event_log.$1:Time) as Timestamp
+            , security_event_log.$1:"Username" as UserName
+            , security_event_log.$1:"Source IP" :: varchar as SourceIP
+            , security_event_log.$1:"Source Port" :: varchar as SourcePort
+            , security_event_log.$1:"Destination IP" :: varchar as DestinationIP
+            , security_event_log.$1:"Destination Port" :: varchar as DestinationPort
+            , security_event_log.$1:"Protocol" :: varchar as Protocol
+        from @s3_stage_json/import/security_event_log.json security_event_log
+    )
     select 
-        security_event_log.$1:"Action" :: varchar as Action
-        , to_timestamp(security_event_log.$1:Date || ' ' || security_event_log.$1:Time) as Timestamp
-        , security_event_log.$1:"Username" as UserName
-        , security_event_log.$1:"Source IP" :: varchar as SourceIP
-        , security_event_log.$1:"Source Port" :: varchar as SourcePort
-        , security_event_log.$1:"Destination IP" :: varchar as DestinationIP
-        , security_event_log.$1:"Destination Port" :: varchar as DestinationPort
-        , security_event_log.$1:"Protocol" :: varchar as Protocol
-    from @s3_stage_json/import/security_event_log.json security_event_log
+        Username
+        , Action 
+        , Timestamp as Timestamp 
+        , dateadd(millisecond, -1, lead(timestamp) over (partition by username order by timestamp)) as end_timestamp
+        , SourceIP 
+        , SourcePort 
+        , DestinationIP 
+        , DestinationPort
+        , Protocol
+    from base
 ;
 
 select * 
