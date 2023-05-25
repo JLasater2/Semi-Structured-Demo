8d4f516dfb5405b52085310ef397b18662869143
JohnD <john.lasater.2@gmail.com>
2023-05-25 00:42:51 -0500

    Change ip parsing to handle ipv6

--- snowflake/06_ip_lookup_ip.sql
+++ snowflake/06_ip_lookup_ip.sql
@@ -1,21 +1,15 @@
 
 -- Takes an IP as varchar and returns an integer representation of the IP 
 -- for use in looking up IPs in an IP range
-create or replace function ip_to_int(ip_address varchar)
-  returns number
+create or replace function parse_ip_demo(ip_address varchar)
+  returns variant  -- because ipv6 addresses are parsed as hex
   as
   $$
-    cast(
-        cast(split(ip_address, '.')[0]  as number) * pow(256, 3) +  -- convert first octet
-        cast(split(ip_address, '.')[1]  as number) * pow(256, 2) +
-        cast(split(ip_address, '.')[2]  as number) * 256 +
-        cast(split(ip_address, '.')[3]  as number)
-        as number
+    coalesce(
+          parse_ip( ip_address, 'INET' ):hex_ipv6
+        , parse_ip( ip_address, 'INET' ):ipv4
     )
+
   $$
   ;
   
-select
-  *
-from ipinfo_free_ip_geolocation_sample.demo.location
-limit 10;
