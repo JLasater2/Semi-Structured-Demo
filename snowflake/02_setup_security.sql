a106261f977af06906bc903992e7655a4219c597
JohnD <john.lasater.2@gmail.com>
2023-05-29 10:29:56 -0500

    Add role and user for query_history example

--- snowflake/02_setup_security.sql
+++ snowflake/02_setup_security.sql
@@ -1,9 +1,10 @@
 -----------------------------------------------------
--- Setup Faker user and role for data generation
+-- Setup users and roles for data generation and demo
 -----------------------------------------------------
 use role useradmin;
-create or replace user faker password = 'fakermaker#'; -- todo - use key pair auth
+create or replace user faker;  --Password configured separately.
 create or replace role faker_read_only;
+create or replace role accounting_analyst;  -- Used for Snowflake account usage portion of demo 
 grant role faker_read_only to user faker;
 grant role faker_read_only to user johnlasater2;
 grant role faker_read_only to role sysadmin;
@@ -21,12 +22,28 @@ use role useradmin;
 create or replace role semi_structured_demo;
 grant role semi_structured_demo to role sysadmin;
 grant role semi_structured_demo to user johnlasater2;
--- setup db & warehouse
+-----------------------------------------------------
+-- Role to be used for demo compromised user 
+-----------------------------------------------------
+use role useradmin;
+create or replace user norman58;  -- For Snowflake account usage demo.  Password configured separately.
+grant role accounting_analyst to user norman58;
+grant role accounting_analyst to role sysadmin;
+grant role accounting_analyst to user johnlasater2;
+use role sysadmin;
+grant usage on warehouse compute_wh to role faker_read_only; 
+-----------------------------------------------------
+-- Setup db & warehouse
+-----------------------------------------------------
 use role sysadmin;
 create or replace database semi_structured_demo;
 grant all on database semi_structured_demo to role semi_structured_demo;
+grant usage on database semi_structured_demo to role accounting_analyst;
 grant usage on warehouse compute_wh to role semi_structured_demo;
+grant usage on warehouse compute_wh to role accounting_analyst;
 use role accountadmin;
+-----------------------------------------------------
 -- Grant access to storage integration
+-----------------------------------------------------
 grant usage on integration s3_integration to role semi_structured_demo; 
 
--- snowflake/99_cleanup.sql
+++ snowflake/99_cleanup.sql
@@ -1,7 +1,9 @@
 use role useradmin;
 drop user if exists faker;
+drop user if exists norman58;
 drop role if exists faker_read_only;
 drop role if exists semi_structured_demo;
+drop role if exists accounting_analyst;
 
 use role sysadmin;
 drop database if exists semi_structured_demo;
